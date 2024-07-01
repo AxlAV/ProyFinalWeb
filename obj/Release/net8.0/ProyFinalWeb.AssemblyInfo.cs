e600e59048f630fe26f51efe7acb30a847347714
AxlAV <axljareb@hotmail.com>
2024-07-01 12:56:26 -0500

    Creacion de APIS

--- obj/Release/net8.0/ProyFinalWeb.AssemblyInfo.cs
+++ obj/Release/net8.0/ProyFinalWeb.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyFinalWeb")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bba6bd94d243d089e770e1487021c3cbdbfbdd11")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyFinalWeb")]
